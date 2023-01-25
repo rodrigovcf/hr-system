f19b990de7af0205087ba8ab95973288f864a162
Rodrigo Valença <rodrigovalenca@MacBook-Pro-de-Rodrigo.local>
2023-01-25 17:19:11 -0300

    Adjust name gatway to gateway

--- api-gatway/src/main/java/br/com/hrservices/apigateway/ApiGatewayApplication.java
+++ api-gatway/src/main/java/br/com/hrservices/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package br.com.hrservices.apigatway;
+package br.com.hrservices.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ApiGatwayApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatwayApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
